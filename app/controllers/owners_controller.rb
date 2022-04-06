a23a16764a4ccd9e21234b6fb26534fe7ca2bb7e
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-06 11:33:26 +0800

    Added controller actions and UI for Owner

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,49 @@
 class OwnersController < ApplicationController
+    def index
+        # needs template
+        @owners = Owner.all
+    end
+    
+    # Create an Owner
+    def new
+        # Needs A Template
+        @owner = Owner.new
+    end
+
+    def create
+        # Creates an instance and redirects
+        @owner = Owner.new(req_params)
+        if @owner.save
+            redirect_to owners_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    # Edit an Owner
+    def edit
+        @owner = Owner.find(params[:id])
+    end
+
+    def update
+        @owner = Owner.find(params[:id])
+        if @owner.update(req_params)
+            redirect_to owners_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    # Delete an Owner
+    def destroy
+        @owner = Owner.find(params[:id])
+        @owner.destroy
+        redirect_to owners_path, status: :see_other
+    end
+    
+    
+    private
+        def req_params
+            params.require(:owner).permit(:name, :tel_num)
+        end
 end

c92357b2d1a8e5e9c702ba1864cface40fcaf126
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-06 12:17:27 +0800

    Pet routed as nested resources + Added its controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :owners
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root "owners#index"
+
+  # Pets as nested resource within Owners
+  resources :owners do
+    resources :pets
+  end
 end

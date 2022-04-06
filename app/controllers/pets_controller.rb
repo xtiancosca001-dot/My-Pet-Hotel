87d24b826af3fce7f2b7954af5d0fe0fb4f506f8
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-06 13:15:35 +0800

    Pet Entries are dealt with (without deleting them yet)

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+    def create
+        @owner = Owner.find(params[:owner_id])
+        @pet = @owner.pets.create(pet_params)
+        redirect_to owner_path(@owner)
+    end
+
+    private
+        def pet_params
+            params.require(:pet).permit(:name, :pet_type)
+        end
 end

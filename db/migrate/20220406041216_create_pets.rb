e6cb8cb6b24e63c950ca51cfabe2582a6a74c9f6
Christian Eric O. Cosca <christiancosca@gmail.com>
2022-04-07 10:18:24 +0800

    issues encountered

--- db/migrate/20220406041216_create_pets.rb
+++ db/migrate/20220406041216_create_pets.rb
@@ -2,7 +2,7 @@ class CreatePets < ActiveRecord::Migration[7.0]
   def change
     create_table :pets do |t|
       t.string :name
-      t.string :type
+      t.string :pet_type
       t.references :owner, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_041216) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "pet_type"
     t.integer "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
